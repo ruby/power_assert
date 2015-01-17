354121194f45dfb76fba147951fc682c82c1fe40
Kazuki Tsujimoto <kazuki@callcc.net>
2015-01-17 12:05:43 +0900

    Update copyright notices for 2015

--- lib/power_assert.rb
+++ lib/power_assert.rb
@@ -1,6 +1,6 @@
 # power_assert.rb
 #
-# Copyright (C) 2014 Kazuki Tsujimoto, All rights reserved.
+# Copyright (C) 2014-2015 Kazuki Tsujimoto, All rights reserved.
 
 begin
   TracePoint.new(:return, :c_return) {}
