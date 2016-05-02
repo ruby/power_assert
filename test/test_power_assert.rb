47541fd7b7a36866c10ea1fb1af448686028b6f7
Kazuki Tsujimoto <kazuki@callcc.net>
2016-05-02 15:28:47 +0900

    Revise tests

--- test/test_power_assert.rb
+++ test/test_power_assert.rb
@@ -4,6 +4,13 @@ require 'ripper'
 require 'set'
 
 class TestPowerAssert < Test::Unit::TestCase
+  class << self
+    def t(msg='', &blk)
+      loc = caller_locations(1, 1)[0]
+      test("#{loc.path} --location #{loc.lineno} #{msg}", &blk)
+    end
+  end
+
   EXTRACT_METHODS_TEST = [
     [[[:method, "c", 4], [:method, "b", 2], [:method, "d", 8], [:method, "a", 0]],
       'a(b(c), d)'],
@@ -148,194 +155,215 @@ class TestPowerAssert < Test::Unit::TestCase
     false
   end
 
-  def test_lazy_inspection
-    PowerAssert.configure do |c|
-      assert !c.lazy_inspection
-    end
-    assert_equal <<END.chomp, assertion_message {
-      'a'.sub(/./, 'b').sub!(/./, 'c')
-          |             |
-          |             "c"
-          "b"
-END
-      'a'.sub(/./, 'b').sub!(/./, 'c')
-    }
-
-    PowerAssert.configure do |c|
-      c.lazy_inspection = true
-    end
-    begin
+  sub_test_case 'lazy_inspection' do
+    t do
+      PowerAssert.configure do |c|
+        assert !c.lazy_inspection
+      end
       assert_equal <<END.chomp, assertion_message {
         'a'.sub(/./, 'b').sub!(/./, 'c')
             |             |
             |             "c"
-            "c"
+            "b"
 END
         'a'.sub(/./, 'b').sub!(/./, 'c')
       }
-    ensure
+    end
+
+    t do
       PowerAssert.configure do |c|
-        c.lazy_inspection = false
+        c.lazy_inspection = true
+      end
+      begin
+        assert_equal <<END.chomp, assertion_message {
+          'a'.sub(/./, 'b').sub!(/./, 'c')
+              |             |
+              |             "c"
+              "c"
+END
+          'a'.sub(/./, 'b').sub!(/./, 'c')
+        }
+      ensure
+        PowerAssert.configure do |c|
+          c.lazy_inspection = false
+        end
       end
     end
   end
 
-  def test_assertion_message
-    a = 0
-    @b = 1
-    @@c = 2
-    $d = 3
-    assert_equal <<END.chomp, assertion_message {
-      String(a) + String(@b) + String(@@c) + String($d)
-      |      |  | |      |   | |      |    | |      |
-      |      |  | |      |   | |      |    | |      3
-      |      |  | |      |   | |      |    | "3"
-      |      |  | |      |   | |      |    "0123"
-      |      |  | |      |   | |      2
-      |      |  | |      |   | "2"
-      |      |  | |      |   "012"
-      |      |  | |      1
-      |      |  | "1"
-      |      |  "01"
-      |      0
-      "0"
+  sub_test_case 'assertion_message' do
+    t do
+      a = 0
+      @b = 1
+      @@c = 2
+      $d = 3
+      assert_equal <<END.chomp, assertion_message {
+        String(a) + String(@b) + String(@@c) + String($d)
+        |      |  | |      |   | |      |    | |      |
+        |      |  | |      |   | |      |    | |      3
+        |      |  | |      |   | |      |    | "3"
+        |      |  | |      |   | |      |    "0123"
+        |      |  | |      |   | |      2
+        |      |  | |      |   | "2"
+        |      |  | |      |   "012"
+        |      |  | |      1
+        |      |  | "1"
+        |      |  "01"
+        |      0
+        "0"
 END
-      String(a) + String(@b) + String(@@c) + String($d)
-    }
+        String(a) + String(@b) + String(@@c) + String($d)
+      }
+    end
 
-
-    assert_equal <<END.chomp, assertion_message {
-      "0".class == "3".to_i.times.map {|i| i + 1 }.class
-          |     |      |    |     |                |
-          |     |      |    |     |                Array
-          |     |      |    |     [1, 2, 3]
-          |     |      |    #<Enumerator: 3:times>
-          |     |      3
-          |     false
-          String
+    t do
+      assert_equal <<END.chomp, assertion_message {
+        "0".class == "3".to_i.times.map {|i| i + 1 }.class
+            |     |      |    |     |                |
+            |     |      |    |     |                Array
+            |     |      |    |     [1, 2, 3]
+            |     |      |    #<Enumerator: 3:times>
+            |     |      3
+            |     false
+            String
 END
-      "0".class == "3".to_i.times.map {|i| i + 1 }.class
-    }
+        "0".class == "3".to_i.times.map {|i| i + 1 }.class
+      }
+    end
 
+    t do
+      assert_equal '', assertion_message {
+        false
+      }
+    end
 
-    assert_equal '', assertion_message {
-      false
-    }
-
-
-    assert_equal <<END.chomp,
-    assertion_message { "0".class }
-                            |
-                            String
+    t do
+      assert_equal <<END.chomp,
+      assertion_message { "0".class }
+                              |
+                              String
 END
-    assertion_message { "0".class }
+      assertion_message { "0".class }
+    end
 
-
-    assert_equal <<END.chomp,
-      "0".class
-          |
-          String
+    t do
+      assert_equal <<END.chomp,
+        "0".class
+            |
+            String
 END
-    Assertion {
-      "0".class
-    }
+      Assertion {
+        "0".class
+      }
+    end
 
-
-    assert_equal <<END.chomp,
-    Assertion { "0".class }
-                    |
-                    String
+    t do
+      assert_equal <<END.chomp,
+      Assertion { "0".class }
+                      |
+                      String
 END
-    Assertion { "0".class }
+      Assertion { "0".class }
+    end
 
-
-    assert_equal <<END.chomp, assertion_message {
-      Set.new == Set.new([0])
-      |   |   |  |   |
-      |   |   |  |   #<Set: {0}>
-      |   |   |  Set
-      |   |   false
-      |   #<Set: {}>
-      Set
+    t do
+      assert_equal <<END.chomp, assertion_message {
+        Set.new == Set.new([0])
+        |   |   |  |   |
+        |   |   |  |   #<Set: {0}>
+        |   |   |  Set
+        |   |   false
+        |   #<Set: {}>
+        Set
 END
-      Set.new == Set.new([0])
-    }
+        Set.new != Set.new([0])
+      }
+    end
 
-
-    var = [10,20]
-    assert_equal <<END.chomp, assertion_message {
-      var[0] == 0
-      |  |   |
-      |  |   false
-      |  10
-      [10, 20]
+    t do
+      var = [10,20]
+      assert_equal <<END.chomp, assertion_message {
+        var[0] == 0
+        |  |   |
+        |  |   false
+        |  10
+        [10, 20]
 END
-      var[0] == 0
-    }
+        var[0] == 0
+      }
+    end
 
-    a = 1
-    assert_equal <<END.chomp, assertion_message {
-      ! a != (+a == -a)
-      | | |   || |  ||
-      | | |   || |  |1
-      | | |   || |  -1
-      | | |   || false
-      | | |   |1
-      | | |   1
-      | | false
-      | 1
-      false
+    t do
+      a = 1
+      assert_equal <<END.chomp, assertion_message {
+        ! a != (+a == -a)
+        | | |   || |  ||
+        | | |   || |  |1
+        | | |   || |  -1
+        | | |   || false
+        | | |   |1
+        | | |   1
+        | | false
+        | 1
+        false
 END
-      ! a != (+a == -a)
-    }
+        ! a != (+a == -a)
+      }
+    end
 
-    assert_equal <<END.chomp, assertion_message {
-      bmethod
-      |
-      false
+    t do
+      assert_equal <<END.chomp, assertion_message {
+        bmethod
+        |
+        false
 END
-      bmethod
-    }
+        bmethod
+      }
+    end
 
-
-    a = :a
-    assert_equal <<END.chomp, assertion_message {
-      a == :b
-      | |
-      | false
-      :a
+    t do
+      a = :a
+      assert_equal <<END.chomp, assertion_message {
+        a == :b
+        | |
+        | false
+        :a
 END
-      a == :b
-    }
+        a == :b
+      }
+    end
 
-
-    assert_equal <<END.chomp, assertion_message {
-      ! Object
-      | |
-      | Object
-      false
+    t do
+      assert_equal <<END.chomp, assertion_message {
+        ! Object
+        | |
+        | Object
+        false
 END
-      ! Object
-    }
-
+        ! Object
+      }
+    end
 
     if PowerAssert.respond_to?(:clear_global_method_cache, true)
-      3.times do
-        assert_equal <<END.chomp, assertion_message {
-          String == Array
-          |      |  |
-          |      |  Array
-          |      false
-          String
+      t do
+        3.times do
+          assert_equal <<END.chomp, assertion_message {
+            String == Array
+            |      |  |
+            |      |  Array
+            |      false
+            String
 END
-          String == Array
-        }
+            String == Array
+          }
+        end
       end
     end
   end
 
-  def test_inspection_failure
-    assert_match Regexp.new(<<END.chomp.gsub('|', "\\|")),
+  sub_test_case 'inspection_failure' do
+    t do
+      assert_match Regexp.new(<<END.chomp.gsub('|', "\\|")),
       assertion_message { BasicObjectSubclass.new.foo }
                           |                   |   |
                           |                   |   "foo"
@@ -343,90 +371,102 @@ END
                           TestPowerAssert::BasicObjectSubclass
 END
       assertion_message { BasicObjectSubclass.new.foo }
-
-
-    o = Object.new
-    def o.inspect
-      raise
     end
-    assert_equal <<END.chomp.b, assertion_message {
-      o.class
-      | |
-      | Object
-      InspectionFailure: RuntimeError:
-END
-      o.class
-    }
-  end
 
-  def test_alias_method
-    PowerAssert.configure do |c|
-      c._trace_alias_method = true
-    end
-    begin
-      o = Class.new do
-        def foo
-          :foo
-        end
-        alias alias_of_iseq foo
-        alias alias_of_cfunc to_s
+    t do
+      o = Object.new
+      def o.inspect
+        raise
       end
-
-      assert_match Regexp.new(<<END.chomp.gsub('|', "\\|")),
-        assertion_message { o.new.alias_of_iseq }
-                            | |   |
-                            | |   :foo
-                            | #<#<Class:.*>:.*>
-                            #<Class:.*>
+      assert_equal <<END.chomp.b, assertion_message {
+        o.class
+        | |
+        | Object
+        InspectionFailure: RuntimeError:
 END
-        assertion_message { o.new.alias_of_iseq }
-
-      omit 'alias of cfunc is not supported yet'
-      assert_match Regexp.new(<<END.chomp.gsub('|', "\\|")),
-        assertion_message { o.new.alias_of_cfunc }
-                            | |   |
-                            | |   #<#<Class:.*>:.*>
-                            | #<#<Class:.*>:.*>
-                            #<Class:.*>
-END
-        assertion_message { o.new.alias_of_cfunc }
-    ensure
-      PowerAssert.configure do |c|
-        c._trace_alias_method = false
-      end
-    end
-  end
-
-  def test_assertion_message_with_incompatible_encodings
-    if Encoding.default_external == Encoding::UTF_8
-      a = "\u3042"
-      def a.inspect
-        super.encode('utf-16le')
-      end
-      assert_equal <<END.chomp, assertion_message {
-        a + a
-        | | |
-        | | "\u3042"(UTF-16LE)
-        | "\u3042\u3042"
-        "\u3042"(UTF-16LE)
-END
-        a + a
+        o.class
       }
     end
+  end
 
-
-    a = "\xFF"
-    def a.inspect
-      "\xFF".force_encoding('ascii-8bit')
+  sub_test_case 'alias_method' do
+    def setup
+      begin
+        PowerAssert.configure do |c|
+          c._trace_alias_method = true
+        end
+        @o = Class.new do
+          def foo
+            :foo
+          end
+          alias alias_of_iseq foo
+          alias alias_of_cfunc to_s
+        end
+        yield
+      ensure
+        PowerAssert.configure do |c|
+          c._trace_alias_method = false
+        end
+      end
     end
-    assert_equal <<END.chomp.b, assertion_message {
-      a.length
-      | |
-      | 1
-      \xFF
+
+    t do
+      assert_match Regexp.new(<<END.chomp.gsub('|', "\\|")),
+        assertion_message { @o.new.alias_of_iseq }
+                            |  |   |
+                            |  |   :foo
+                            |  #<#<Class:.*>:.*>
+                            #<Class:.*>
 END
-      a.length
-    }.b
+        assertion_message { @o.new.alias_of_iseq }
+    end
+
+    t do
+      omit 'alias of cfunc is not supported yet'
+      assert_match Regexp.new(<<END.chomp.gsub('|', "\\|")),
+        assertion_message { @o.new.alias_of_cfunc }
+                            |  |   |
+                            |  |   #<#<Class:.*>:.*>
+                            |  #<#<Class:.*>:.*>
+                            #<Class:.*>
+END
+        assertion_message { @o.new.alias_of_cfunc }
+    end
+  end
+
+  sub_test_case 'assertion_message_with_incompatible_encodings' do
+    if Encoding.default_external == Encoding::UTF_8
+      t do
+        a = "\u3042"
+        def a.inspect
+          super.encode('utf-16le')
+        end
+        assert_equal <<END.chomp, assertion_message {
+          a + a
+          | | |
+          | | "\u3042"(UTF-16LE)
+          | "\u3042\u3042"
+          "\u3042"(UTF-16LE)
+END
+          a + a
+        }
+      end
+    end
+
+    t do
+      a = "\xFF"
+      def a.inspect
+        "\xFF".force_encoding('ascii-8bit')
+      end
+      assert_equal <<END.chomp.b, assertion_message {
+        a.length
+        | |
+        | 1
+        \xFF
+END
+        a.length
+      }.b
+    end
   end
 
   def test_assertion_message_with_string
